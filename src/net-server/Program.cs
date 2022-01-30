f349135b47b7ac9a9dcdc34e559fdb22e3de843a
Fernando Escolar <f.escolar@hotmail.com>
2022-01-30 18:40:02 +0100

    cleaning garbage

--- src/net-server/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-{
-    Args = args,
-    // Look for static files in dist
-    WebRootPath = "dist"
-});
-
-var app = builder.Build();
-app.Run();
